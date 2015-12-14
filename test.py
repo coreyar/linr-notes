48ff6fe69b79edb47ffe77e68a7453d7d68f00e1
CoreyAR <coreyarice@gmail.com>
2015-12-14 16:58:40 -0600

    Added 404 template, updated test case

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class FlaskTestCase(unittest.TestCase):
 
 	def test_index(self):
 		tester = app.test_client(self)
-		response = tester.get('/login', content_type='html/text')
+		response = tester.get('/', content_type='html/text')
 		self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
