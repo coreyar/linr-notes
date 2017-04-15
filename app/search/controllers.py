849bde023f97ced64fd4a7e1af59dc6bf992908c
Waitsfornon <enders.tim@gmail.com>
2017-04-15 14:27:01 -0500

    updated controller arg parsing

--- app/search/controllers.py
+++ app/search/controllers.py
@@ -3,12 +3,12 @@ from spotify_query import spotify_track
 
 import os, urllib
 
-parser = reqparse.RequestParser()
-parser.add_argument('track', type=str, help'Spotify Song/Track URI')
-args = parser.parse_args()
+
 
 class Search(Resource):
     def get(self, track):
-        # Default to 200 OK
+        parser = reqparse.RequestParser()
+        parser.add_argument('track', type=str, help='Spotify Song/Track URI')
+        args = parser.parse_args()
         return spotify_track(track)
 
