e03079b2f9c07c5852ec931fcd253c84a8e5a191
Tim Timandsarah Sarah <timandsarah@Tims-MacBook-Pro.local>
2017-04-15 14:08:38 -0500

    added endpoint for search to controller

--- app/search/controllers.py
+++ app/search/controllers.py
@@ -1,11 +1,14 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from spotify_query import spotify_track
 
 import os, urllib
 
+parser = reqparse.RequestParser()
+parser.add_argument('track', type=str, help'Spotify Song/Track URI')
+args = parser.parse_args()
+
 class Search(Resource):
-    def post(self):
+    def get(self, track):
         # Default to 200 OK
-        return {'task': 'Hello world'}
-
-
+        return spotify_track(track)
 
